87661c1476a147a99645f7e3742f168e7486af66
Siam Ahmed <siamahmed.only@gmail.com>
2026-02-19 16:28:59 +0600

    update

--- coffee-store-client/src/main.jsx
+++ coffee-store-client/src/main.jsx
@@ -1,20 +1,40 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import MainLayout from './layout/MainLayout';
+import Home from './components/Home';
+import UpdateCoffee from './components/UpdateCoffee';
+import AddCoffee from './components/AddCoffee';
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home
+      },
+
+      {
+        path: "addCoffee",
+        Component: AddCoffee
+      },
+
+      {
+        path: "updateCoffee",
+        Component: UpdateCoffee
+      },
+    ]
   },
 ]);
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />,
+    <RouterProvider router={router} />
   </StrictMode>,
 )
